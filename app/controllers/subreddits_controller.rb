5c7ae112a6ff3657b087339907a2d92bd5d2341f
Christian Carey <christianrcarey@gmail.com>
2017-02-14 17:55:37 -0600

    render json from subreddit ctrl

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -3,6 +3,9 @@ class SubredditsController < ApplicationController
   def show
     @subreddit = Subreddit.find_by(id: params[:id])
     @children = @subreddit.destination_subreddits
+    respond_to do |format|
+      format.json
+    end
   end
 
 end
