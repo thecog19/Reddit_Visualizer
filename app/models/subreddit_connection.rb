8993ef907a9dffe2647495fd17f70e0ef87f4a1c
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-14 10:33:12 -0500

    Implement SubredditConnection uniqueness validator

--- app/models/subreddit_connection.rb
+++ app/models/subreddit_connection.rb
@@ -1,3 +1,4 @@
+
 class SubredditConnection < ApplicationRecord
   belongs_to :subreddit_from,
     foreign_key: "subreddit_from_id",
@@ -5,4 +6,16 @@ class SubredditConnection < ApplicationRecord
   belongs_to :subreddit_to,
     foreign_key: "subreddit_to_id",
     class_name: "Subreddit"
+
+  validate :unique_both_directions
+
+  def unique_both_directions
+    if SubredditConnection.exists?(subreddit_from_id: subreddit_from_id,
+        subreddit_to_id: subreddit_to_id) ||
+       SubredditConnection.exists?(subreddit_from_id: subreddit_to_id,
+                                   subreddit_to_id: subreddit_from_id)
+      errors.add(:connection, "must be unique in both directions")
+    end
+  end
+
 end
