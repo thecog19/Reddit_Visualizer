2ecfbbaf2f4859662ed4f12ac5ef4751ec5c9038
Christian Carey <christianrcarey@gmail.com>
2017-02-14 18:02:45 -0600

    handle missing subreddit in ctrl

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -2,9 +2,11 @@ class SubredditsController < ApplicationController
 
   def show
     @subreddit = Subreddit.find_by(id: params[:id])
-    @children = @subreddit.destination_subreddits
-    respond_to do |format|
-      format.json
+    if @subreddit 
+      @children = @subreddit.destination_subreddits
+      respond_to do |format|
+        format.json
+      end
     end
   end
 
