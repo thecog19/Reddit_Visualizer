3b8ab01ffeb4e1dd20788f4c17cf38dd5f71db34
Francisco Suero <suerofrancisco@yahoo.com>
2017-02-22 12:17:33 -0500

    tree building

--- pathfinder.rb
+++ pathfinder.rb
@@ -11,17 +11,39 @@ class Pathfinder
     unless end_subreddit
       'Node does not exist'
     else
-      queue += start_subreddit.get_top_connections(limit)
+      queue += add_nodes(start_subreddit, limit)
       until queue.empty?
         node = queue.shift
-        next if subreddits[node.id]
-        next if node.subscriber_count > 11_000_000
-        puts node.name
-        subreddits[node.id] = true
-        queue += node.get_top_connections(limit)
-        return if node == end_subreddit
+        next if subreddits[node.subreddit.id]
+        next if node.subreddit.subscriber_count > 11_000_000
+        puts node.subreddit.name
+        subreddits[node.subreddit.id] = true
+        queue += add_nodes(node.subreddit, limit)
+        path_array(node, start_subreddit) if node.subreddit == end_subreddit
       end
     end
+    "No path found"
+  end
+
+  def add_nodes(subreddit, limit)
+    return_array = []
+    subreddit.get_top_connections(limit).each do |sub|
+      sub_hash = {} 
+      sub_hash.subreddit = sub
+      sub_hash.parent = subreddit
+      return_array.push(sub_hash) 
+    end
+    return_array
+  end
+
+  def path_array(node, start_subreddit)
+    current_node = node
+    path_array = []
+    until  current_node == start_subreddit
+      path_array.push(current_node)
+      current_node = current_node.parent
+    end 
+    path_array
   end
 end
 pf = Pathfinder.new
