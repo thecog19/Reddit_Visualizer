125c0d08a39a5b0f629869e5157df1d063160696
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2017-02-22 10:40:17 -0500

    Refactor SubredditPersister
    Pass offset control to reddit_api gem

--- app/models/subreddit_persister.rb
+++ app/models/subreddit_persister.rb
@@ -1,26 +1,16 @@
 
 class SubredditPersister
-  attr_reader :captured, :failures
-
-  CAPTURE_RATE = 100
-  DEFAULT_REQUEST_LIMIT = 2
 
   def initialize(args = {})
     @subreddit_api = args.fetch(:subreddit_api, RedditApi::Subreddits.new)
-    @captured = 0
-    @captured_this_iteration = 0
-    @failures = args.fetch(:failures, 0)
-    @request_limit = args.fetch(:limit, DEFAULT_REQUEST_LIMIT)
     @connector = SubredditConnector.new
+    @subreddit_factory = Subreddit
+    @connection_factory = SubredditConnection
   end
 
   def collect_subreddits(count)
-    while captured < count && failures < request_limit
-      subreddits_data = subreddit_api.top(count)
-      persist_subreddits(subreddits_data, count)
-      update_failures
-    end
-    reset_collection_metrics
+    subreddits_data = subreddit_api.top(count)
+    persist_subreddits(subreddits_data, count)
   end
 
   def collect_subreddit_connections(user_count = 5)
@@ -31,38 +21,18 @@ class SubredditPersister
     end
   end
 
-  protected
-  attr_accessor :captured_this_iteration
-  attr_writer :captured, :failures
   private
-  attr_reader :subreddit_api, :connector, :request_limit
+  attr_reader :subreddit_api, :connector, :subreddit_factory,
+              :connection_factory
 
   def persist_subreddits(subreddits_data, count)
-    self.captured_this_iteration = 0
     subreddits_data.each do |subreddit_data|
       persist_subreddit(subreddit_data)
-      break if captured == count
     end
   end
 
   def persist_subreddit(subreddit_data)
-    subreddit = Subreddit.new(subreddit_data.to_h)
-    if subreddit.valid?
-      subreddit.save
-      self.captured += 1
-      self.captured_this_iteration += 1
-    end
-  end
-
-  def update_failures
-    if captured_this_iteration.zero?
-      self.failures += 1
-    end
-  end
-
-  def reset_collection_metrics
-    self.captured = 0
-    self.failures = 0
+    subreddit_factory.create(subreddit_data.to_h)
   end
 
   def persist_subreddit_connections(connections)
@@ -87,6 +57,5 @@ class SubredditPersister
   def update_parent_subreddit(subreddit)
     subreddit.update(children_added_at: Time.now)
   end
-
 end
 
