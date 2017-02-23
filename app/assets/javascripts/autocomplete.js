c3ff53ef30c49575a0b3e8b3d6300d0de7db7a83
Jonathan Popenuck <john.popenuck@gmail.com>
2017-02-23 10:10:49 -0800

    rendering complete, working on typeahead, fixed nil parent on associations

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -17,6 +17,7 @@ var substringMatcher = function(strs) {
 };
 
 var subreddits = [];
+var search_reddits = [];
 $.ajax( { url: 'api/v1/subreddits.json',
         dataType: 'json',
         success: function(response) {extract_names(response)},
@@ -28,10 +29,11 @@ $.ajax( { url: 'api/v1/subreddits.json',
 var extract_names = function(response) {
   for (var i = 0; i < response.length; i++) {
     subreddits.push(response[i].name)
+    search_reddits.push(response[i].name)
   }
 }
 
-$(window).ready(function() {
+$(document).ready(function() {
   $('#the-basics .typeahead').typeahead({
     hint: true,
     highlight: true,
@@ -40,5 +42,14 @@ $(window).ready(function() {
   {
     name: 'subreddits',
     source: substringMatcher(subreddits)
+  });
+  $('#path-input .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: 'search_reddits',
+    source: substringMatcher(search_reddits)
   });
 })
